dac4777245106b4f049f83c7cebbf032fb3263a9
Voltranger <pranav.101205mahajan@gmail.com>
2026-02-09 20:15:43 +0530

    frontend

---
+++ Frontend/src/components/Navbar.jsx
@@ -0,0 +1,10 @@
+function Navbar()
+{
+    return (
+    <nav className="navbar">
+      <h2>FineRcom</h2>
+      <button>Get Recommendation</button>
+    </nav>
+  );
+}
+export default Navbar;
---
+++ Frontend/src/pages/Home.jsx
@@ -0,0 +1,18 @@
+import Navbar from '../components/Navbar';
+import Hero from '../components/Hero';
+import HowItWorks from '../components/HowItWorks';
+import Footer from '../components/Footer';
+
+
+function Home() {
+  return (
+    <div>
+      <Navbar />
+      <Hero />
+      <HowItWorks />
+      <Footer />
+    </div>
+  );
+}
+
+export default Home;
