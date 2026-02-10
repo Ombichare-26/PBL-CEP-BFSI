6fe7331285a8d5a68128b7b4f0cafc42ede0729d
Voltranger <pranav.101205mahajan@gmail.com>
2026-02-10 19:25:50 +0530

    error fixing

--- Backend/src/utils/apiError.js
+++ Backend/src/utils/apiError.js
@@ -1,24 +1,27 @@
-class ApiErrors extends Error{
- constructor(
-    statusCode, message = "Something went wrong",
-    errors = [],
-    stack = ""
- ){
-    super(message);
-    this.message = message;
-    this.statusCode = statusCode;
-    this.success = false;
-    this.data = null;
-    this.errors = errors
+class ApiError extends Error {
+    constructor(
+        statusCode,
+        message = "Something went Wrong",
+        errors = [],
+        stack = ""
+    ) {
+        super(message)
+        this.statusCode = statusCode
+        this.message = message
+        this.data = null
+        this.success = false
+        this.errors = errors
 
-    if(stack){
-        this.stack = stack;
+
+        if(stack)
+        {
+            this.stack=stack
+        }
+        else{
+            Error.captureStackTrace(this,this.constructor)
+        }
     }
 
-    else{
-        Error.captureStackTrace(this, this.constructor);
-    }
- }
 }
 
-export {ApiErrors}
+export {ApiError}
