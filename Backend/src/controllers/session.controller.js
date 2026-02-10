9e700b1718ab3bc1db2fe5d05c1a14c2871bf5cb
Voltranger <pranav.101205mahajan@gmail.com>
2026-02-10 20:26:26 +0530

    Refactor createSession function for improved readability and error handling

--- Backend/src/controllers/session.controller.js
+++ Backend/src/controllers/session.controller.js
@@ -1,29 +1,25 @@
+import { asyncHandler } from "../utils/asyncHandler.js";
+import { ApiError } from "../utils/ApiError.js";
+import { ApiResponse } from "../utils/ApiResponse.js";
+import { Session } from "../models/Session.model.js";
 
-import {asyncHandler} from "../utils/asyncHandler.js"
-import {ApiError} from "../utils/ApiError.js"
-import {ApiResponse} from "../utils/ApiResponse.js"
-import { Session } from "../models/Session.model.js"
+export const createSession = asyncHandler(async (req, res) => {
+  const expiresAt = new Date();
+  expiresAt.setHours(expiresAt.getHours() + 2);
 
+  const session = await Session.create({
+    expires_at: expiresAt,
+  });
 
+  if (!session) {
+    throw new ApiError(500, "Failed to create session");
+  }
 
-export const createSession=asyncHandler(async(req,res)=>{
-    const expiresAt=new Date();
-    expiresAt.setHours(expiresAt,getHours()+2);
-
-
-    const session=await Session.create({
-        expires_at:expiresAt
-    });
-
-    if(!session)
-    {
-        throw new ApiError(500,"failed to create session")
-    }
-
-    return res.status(201).json(
-        new ApiResponse(201,{
-            session_id:session._id
-        },"Session created successfully")
-        
-    );
+  return res.status(201).json(
+    new ApiResponse(
+      201,
+      { session_id: session._id },
+      "Session created successfully"
+    )
+  );
 });
