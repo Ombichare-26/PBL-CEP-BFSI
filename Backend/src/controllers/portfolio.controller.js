3345dfd219f29385aa98997385edefebdb0e7747
Voltranger <pranav.101205mahajan@gmail.com>
2026-02-11 14:07:43 +0530

    minor changes

--- Backend/src/controllers/portfolio.controller.js
+++ Backend/src/controllers/portfolio.controller.js
@@ -4,26 +4,16 @@ import { ApiResponse } from "../utils/ApiResponse.js";
 import UserPortfolio from "../models/User_Portfolio.model.js";
 
 const saveUserPortfolio = asyncHandler(async (req, res) => {
-  const { session_id, scheme_name, units, amfi_code } = req.body;
+  const { session_id, funds } = req.body;
 
   if (!session_id) {
     throw new ApiError(400, "Session ID is required");
   }
 
-  if (!scheme_name || units === undefined) {
-    throw new ApiError(400, "Scheme name and units are required");
-  }
+  
+  
 
-  const portfolioInput = await UserPortfolio.create({
-    session_id,
-    scheme_name,
-    units,
-    amfi_code // optional
-  });
-
-    if (!portfolioInput) {
-    throw new ApiError(500, "Failed to save portfolio data");
-    }
+    
 
     return res.status(201).json(
     new ApiResponse(
