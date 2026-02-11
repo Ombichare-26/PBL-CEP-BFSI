314b5a4713876e1757fb30fc4511126d0b60bbed
Voltranger <pranav.101205mahajan@gmail.com>
2026-02-11 14:59:17 +0530

    portfolio controller

--- Backend/src/controllers/portfolio.controller.js
+++ Backend/src/controllers/portfolio.controller.js
@@ -10,18 +10,42 @@ const saveUserPortfolio = asyncHandler(async (req, res) => {
     throw new ApiError(400, "Session ID is required");
   }
 
-  
+  if(!Array.isArray(funds) || funds.length==0)
+  {
+    throw new ApiError("PORTFOLIO INPUT FAILED!! Fund array is required!!")
+  }
+
+  const insertedFunds=[];
+
+  for(const fund of funds)
+  {
+    if(!fund.scheme_name || fund.units==undefined)
+    {
+      throw new ApiError("NULL values found for scheme name or units..those values are required")
+    }
+
+   
+
+    await UserPortfolio.create(
+      {
+        session_id,
+        scheme_name:fund.scheme_name,
+        units:fund.units,
+        amfi_code:fund.amfi_code,
+      });
+    
+    insertedFunds.push(fund);
+  }
   
 
-    
 
     return res.status(201).json(
     new ApiResponse(
       201,
-      portfolioInput,
+      insertedFunds,
       "CAS portfolio data saved successfully"
-    )
-  );
-});
+    ));
+  
+  });
 
 export { saveUserPortfolio };
