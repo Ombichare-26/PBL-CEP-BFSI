5f6b84560b450d3926da6b112892d70ccd7f8214
Voltranger <pranav.101205mahajan@gmail.com>
2026-03-04 13:58:19 +0530

    deleted redundant files

--- Frontend/src/services/yahooService.PortfolioPage.jsx
+++
@@ -1,8 +0,0 @@
-import axios from "axios";
-
-const BASE_URL = "http://localhost:8000/api/v1";
-
-export const getFundDetails = async (ticker) => {
-  const res = await axios.get(`${BASE_URL}/fund/${ticker}`);
-  return res.data;
-};
